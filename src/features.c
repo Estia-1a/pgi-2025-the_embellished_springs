a1a9eba3db0537fecef8100285d273b063f54839
Enzolangrand <190848808+Enzolangrand@users.noreply.github.com>
2025-05-16 15:18:04 +0200

    programme qui bug !!!!!

--- src/features.c
+++ src/features.c
@@ -1,17 +1,11 @@
 #include <estia-image.h>
 #include <stdio.h>
-
 #include "features.h"
 #include "utils.h"
 
-/**
- * @brief Here, you have to code features of the project.
- * Do not forget to commit regurlarly your changes.
- * Your commit messages must contain "#n" with: n = number of the corresponding feature issue.
- * When the feature is totally implemented, your commit message must contain "close #n".
- */
-
-void helloWorld() {
-    printf("Hello World !");
+void first_pixel(char *source_path) {
+    unsigned char *data;
+    int width, height, channel_count;
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+    printf("first_pixel: %d, %d, %d\n", data[0], data[1], data[2]);
 }
-
