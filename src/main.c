c71c970d6b48d175f1d2f28a65480533b939c15b
Enzolangrand <190848808+Enzolangrand@users.noreply.github.com>
2025-05-16 15:24:37 +0200

    programme compliqué

--- src/main.c
+++ src/main.c
@@ -9,32 +9,14 @@
 #include "utils.h"
 #include "argsparse.h"
 
+
 int main(int argc, char **argv) {
-  printf("Hello Enzo\n");
-  /*To use debug mode: 
-   - add --debug: freud.exe --debug -f images/input/image.jpeg
-   or 
-   - Simply run your compiled project with start button on the blue menu bar at the bottom of the VS Code window.
-   */
+  struct Arguments configuration;
+  parse_arguments(argc, argv, &configuration);
 
-  /*DO NOT EDIT THIS PART*/
-  Config configuration ;
-  parse_arguments( argc, argv, &configuration ) ;
-  check_debug_mode(configuration);
-  check_file();
-  /* END */
-
-  /* Use "if ( strncmp( command, <commandname>, 9 ) == 0 )" to check if your <commandname> is called by program.*/
-  /* Example with helloworld command
-   * If helloworld is a called command: freud.exe -f images/input/image.jpeg -c helloworld 
-   */
-  if ( strncmp( configuration.command, "helloworld", 10 ) == 0 ) {
-    /* helloworld() function is defined in feature.h and implemented in feature.c */
-    helloWorld();
+  if ( strncmp(configuration.command, "first_pixel", 11) == 0 ) {
+    first_pixel(configuration.file_path);
   }
-  /*
-   * TO COMPLETE
-   */
-  
+
   return 0;
 }
