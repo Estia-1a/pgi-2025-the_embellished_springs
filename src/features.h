3e9ac0ad00f2a6971498c3969f86fe046a47b239
Enzolangrand <190848808+Enzolangrand@users.noreply.github.com>
2025-06-20 00:01:37 +0200

    color

--- src/features.h
+++ src/features.h
@@ -6,8 +6,5 @@ void color_desaturate(char *source_path)
 void color_gray_luminance(char* source_path)
 void mirror_vertical(char* source_path)
 void mirror_horizontal(char* source_path)
-<<<<<<< HEAD
-void color_blue(char* source_path)
-=======
 void color_gray(char* source_path) 
 >>>>>>> 5a86b93280b57709bbf631afb3f8c54c353b5fb9
